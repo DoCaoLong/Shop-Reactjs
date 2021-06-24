f4401012b1aa428f2147b9e379f7045991026d74
DoCaoLong <bmt.long.faker@gmail.com>
2021-06-24 15:16:14 +0700

    map

--- src/page/storeLocation/component/ListCardLocation.jsx
+++ src/page/storeLocation/component/ListCardLocation.jsx
@@ -1,18 +1,18 @@
-export default function ListCardLocation({ address, name, phone, store_hours, link_googlemap }) {
+export default function ListCardLocation({ address, name, phone, store_hours, Clicked, active }) {
 	return (
-		<div className="card-body">
+		<div className={`card-body ${active ? 'active-local' : ''}`} onClick={Clicked}>
 			{/* Heading */}
 			<p className="font-weight-bold">{name}</p>
-			<p className="text-gray-500">{address}</p>
+			<p className={`text-gray-500 ${active ? 'color-white' : ''}`}>{address}</p>
 			<p>
 				<strong>Phone:</strong> <br />
-				<a className="text-gray-500" href="tel:6-146-389-574">
+				<a className={`text-gray-500 ${active ? 'color-white' : ''}`} href="tel:6-146-389-574">
 					{phone}
 				</a>
 			</p>
 			<p className="mb-0">
 				<strong>Store Hours:</strong> <br />
-				<span className="text-gray-500">{store_hours}</span>
+				<span className={`text-gray-500 ${active ? 'color-white' : ''}`}>{store_hours}</span>
 			</p>
 		</div>
 	);
--- src/page/storeLocation/index.jsx
+++ src/page/storeLocation/index.jsx
@@ -5,10 +5,18 @@ import ListCardLocation from './component/ListCardLocation';
 
 export default function StoreLocation() {
 	let [list, setList] = useState([]);
+	let [index, setIndex] = useState(0);
+	let [map, setMap] = useState(
+		'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.2816663015815!2d106.69721651480097!3d10.789725892312536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317528caafa7703d%3A0xd655aeb0c7040512!2zMTEgUGhhbiBL4bq_IELDrW5oLCDEkGEgS2FvLCBRdeG6rW4gMSwgVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5o!5e0!3m2!1svi!2s!4v1624436027992!5m2!1svi!2s'
+	);
 	useEffect(() => {
 		let res = localStore.local().then((res) => setList(res.data));
-		// console.log(list);
 	}, []);
+
+	const HandleChange = (i) => {
+		setIndex(i);
+		setMap(list[i].link_googlemap);
+	};
 	return (
 		<>
 			{/* BREADCRUMB */}
@@ -65,14 +73,21 @@ export default function StoreLocation() {
 							>
 								{/*e la item trong list, i la vi tri item */}
 								{list.map((e, i) => (
-									<ListCardLocation {...e} key={i} />
+									// <ListCardLocation {...e} key={i} Clicked={() => HandleChange(e.name)} />
+									// bind(con trỏ this, )
+									<ListCardLocation
+										{...e}
+										key={i}
+										active={index === i}
+										Clicked={HandleChange.bind(null, i)}
+									/>
 								))}
 							</div>
 						</div>
 						<div className="col-12 col-md-7 col-lg-8">
 							{/* Map */}
 							<div className="embed-responsive embed-responsive-4by3">
-								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.2816663015815!2d106.69721651480097!3d10.789725892312536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317528caafa7703d%3A0xd655aeb0c7040512!2zMTEgUGhhbiBL4bq_IELDrW5oLCDEkGEgS2FvLCBRdeG6rW4gMSwgVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5o!5e0!3m2!1svi!2s!4v1624436027992!5m2!1svi!2s"></iframe>
+								<iframe title="none" src={map}></iframe>
 							</div>
 						</div>
 					</div>
